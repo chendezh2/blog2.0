ad40515f403cd960a2fb9b9078e8435a61c3d9e3
user1 <email1@admin.com>
2015-04-15 13:34:12 +0800

    修改

--- blog.sql
+++ blog.sql
@@ -3,6 +3,7 @@ CREATE TABLE `attachment` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `blog_id` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '所属博客id',
   `path` varchar(255) NOT NULL DEFAULT '' COMMENT '附件路径',
+  `origin_name` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '原始文件名',
   `creation_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '创建时间',
   `modification_time` int(11) unsigned NOT NULL DEFAULT '0' COMMENT '修改时间',
   `is_deleted` tinyint(1) unsigned NOT NULL DEFAULT '0' COMMENT '是否被删除',
