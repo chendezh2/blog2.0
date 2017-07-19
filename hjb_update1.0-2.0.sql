730d914c68119757b3092ac29f6bd45c92848fc2
user1 <email1@admin.com>
2017-07-19 09:44:03 +0800

    hjb_update1.0-2.0

--- hjb_update1.0-2.0.sql
+++ hjb_update1.0-2.0.sql
@@ -1,3 +1,4 @@
+#appapi使用的app和secret机制的表，并插入一条当前使用的记录
 CREATE TABLE `appapi` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(50) NOT NULL COMMENT 'app名称',
@@ -11,5 +12,20 @@ COLLATE='utf8_general_ci'
 ENGINE=InnoDB;
 
 INSERT INTO `appapi` (`id`, `name`, `secret`, `create_time`) VALUES (1, 'hjb9472', 'baf3d1c607bac8620dfaadbf2803dafc', 1471189810);
+
+#member表添加一个字段，用于自动登录时的认证
+ALTER TABLE `member` ADD COLUMN `auth_key` VARCHAR(32) NOT NULL DEFAULT '' COMMENT '自动登录的认证key' AFTER `password`;
+
+
+
+
+
+
+
+
+
+
+
+
 
 
