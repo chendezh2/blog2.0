97ef7e08ec1281cb5208b2c77ed5ead107f3a765
user1 <email1@admin.com>
2017-07-19 17:07:40 +0800

    hjb_update1.0-2.0

--- hjb_update1.0-2.0.sql
+++ hjb_update1.0-2.0.sql
@@ -24,6 +24,25 @@ ALTER TABLE `error_log`
 	ADD COLUMN `post` LONGTEXT NOT NULL COMMENT 'post参数' AFTER `get`,
 	ADD COLUMN `cookie` TEXT NOT NULL COMMENT 'cookie参数' AFTER `post`;
 
+#已删除的用户表
+CREATE TABLE `member_deleted` (
+	`id` INT(11) NOT NULL,
+	`name` VARCHAR(50) NOT NULL COMMENT '用户名(手机号)',
+	`password` VARCHAR(32) NOT NULL COMMENT '登录密码',
+	`auth_key` VARCHAR(32) NOT NULL DEFAULT '' COMMENT '自动登录的认证key',
+	`email` VARCHAR(100) NOT NULL DEFAULT '' COMMENT '用户email',
+	`status` ENUM('1','2') NOT NULL DEFAULT '1' COMMENT '用户状态：1-关闭、2-活动',
+	`desc` VARCHAR(255) NOT NULL DEFAULT '' COMMENT '用户说明',
+	`location` VARCHAR(20) NOT NULL DEFAULT '' COMMENT '所在城市',
+	`create_time` INT(11) NOT NULL DEFAULT '0' COMMENT '创建时间',
+	`last_login_time` INT(11) NOT NULL DEFAULT '0' COMMENT '最后登录时间',
+	`delete_time` INT(11) NOT NULL DEFAULT '0' COMMENT '删除时间',
+	PRIMARY KEY (`id`),
+	INDEX `idx_name` (`name`)
+)
+COMMENT='已删除的用户表'
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB;
 
 
 
